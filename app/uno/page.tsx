e300d7ef0b3c769ef5ba2f7fc991bcb33a63f529
sdtibata <giotibata@gmail.com>
2026-05-04 03:20:34 +0000

    contexto en uno

--- app/uno/page.tsx
+++ app/uno/page.tsx
@@ -1,10 +1,16 @@
+"use client";
 import Props from './props';
+import { useAppContext } from "../contexto/AppContext";
 
 export default function Uno() {
+  const { nombre } = useAppContext();
+
   return (
     <div>
       uno
-      <Props nombre="props" />
+
+      <Props nombre="este es un props" />
+      {nombre}
     </div>
   );
 }
